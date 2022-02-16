3322b55170f30531e75f06fd73fd4e0215dd986b
Daniel Linné <daniel.linne.dl@gmail.com>
2022-02-16 09:53:32 +0100

    Devicetree warnings gone

--- arch/aurix/include/kernel_arch_func.h
+++ arch/aurix/include/kernel_arch_func.h
@@ -7,4 +7,16 @@ arch_thread_return_value_set(struct k_thread *thread, unsigned int value)
 	thread->arch.swap_return_value = value;
 }
 
+
+#ifndef _ASMLANGUAGE
+static ALWAYS_INLINE void arch_kernel_init(void)
+{
+	//TODO: Define correctly
+}
+
+static inline bool arch_is_in_isr(void)
+{
+	return true;//TODO: Define correctly
+}
+#endif
 #endif /* ZEPHYR_ARCH_AURIX_INCLUDE_KERNEL_ARCH_FUNC_H_ */
--- include/arch/aurix/arch.h
+++ include/arch/aurix/arch.h
@@ -2,11 +2,42 @@
 #define ZEPHYR_INCLUDE_ARCH_AURIX_ARCH_H_
 
 #include <arch/aurix/thread.h>
+#include <arch/common/ffs.h>
+#include <arch/common/sys_bitops.h>
+#include <devicetree.h>
 /* Not sure about value.
 */
-#define ARCH_STACK_PTR_ALIGN  0
+#define ARCH_STACK_PTR_ALIGN  0//TODO: Define correctly
 
 typedef struct __esf z_arch_esf_t;
 
 #endif
 
+static inline uint64_t arch_k_cycle_get_64(void){
+	return 0;//TODO: Define correctly
+}
+
+static inline uint32_t arch_k_cycle_get_32(void)
+{
+	return 0;//TODO: Define correctly
+}
+
+static ALWAYS_INLINE void arch_irq_unlock(unsigned int key)
+{
+	//TODO: Define correctly
+}
+
+static ALWAYS_INLINE bool arch_irq_unlocked(unsigned int key)
+{
+	return true; //TODO: Define correctly
+}
+
+static ALWAYS_INLINE unsigned int arch_irq_lock(void)
+{
+
+	return 0;//TODO: Define correctly
+}
+static ALWAYS_INLINE void arch_nop(void)
+{
+	//TODO: Define correctly
+}

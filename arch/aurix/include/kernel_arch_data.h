45bb65969ceeae9305ee93b00c1724f71031e9d1
Daniel Linné <daniel.linne.dl@gmail.com>
2022-02-16 10:56:25 +0100

    init.c internal compiler error gone

---
+++ arch/aurix/include/kernel_arch_data.h
@@ -0,0 +1,15 @@
+#ifndef ZEPHYR_ARCH_ARC_INCLUDE_KERNEL_ARCH_DATA_H_
+#define ZEPHYR_ARCH_ARC_INCLUDE_KERNEL_ARCH_DATA_H_
+
+#include <toolchain.h>
+#include <linker/sections.h>
+#include <arch/cpu.h>
+#include <vector_table.h>
+
+#ifndef _ASMLANGUAGE
+#include <kernel.h>
+#include <zephyr/types.h>
+#include <sys/util.h>
+#include <sys/dlist.h>
+#endif
+#endif

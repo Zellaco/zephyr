260e1293ea391f1c0277a16a417c7877ee55f8ac
Daniel Linné <daniel.linne.dl@gmail.com>
2022-02-23 11:41:08 +0100

    commented away code that sets --orphan-handling=warn, not sure how to do this better

--- cmake/linker/ld/target_baremetal.cmake
+++ cmake/linker/ld/target_baremetal.cmake
@@ -27,9 +27,9 @@ macro(toolchain_ld_baremetal)
   # without any warnings or errors, which is the default behavior.
   # So there is no need to explicitly set a linker flag.
   if(CONFIG_LINKER_ORPHAN_SECTION_WARN)
-    zephyr_ld_options(
-      ${LINKERFLAGPREFIX},--orphan-handling=warn
-    )
+    #zephyr_ld_options(
+    #  ${LINKERFLAGPREFIX},--orphan-handling=warn
+    #)
   elseif(CONFIG_LINKER_ORPHAN_SECTION_ERROR)
     zephyr_ld_options(
       ${LINKERFLAGPREFIX},--orphan-handling=error
